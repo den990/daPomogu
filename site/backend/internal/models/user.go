2806d70b81be3948c41f501223de170311eb24f2
den990 <koldyrev98@icloud.com>
2025-03-07 16:05:21 +0300

    backend: Регистрация волонтера

--- site/backend/internal/models/user.go
+++ site/backend/internal/models/user.go
@@ -3,28 +3,34 @@ package models
 import (
 	"backend/internal/db"
 	"backend/internal/utils"
+	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgconn"
+	"log"
+	"time"
 )
 
 type User struct {
-	ID           uint   `gorm:"primary_key"`
-	Email        string `gorm:"unique;not null"`
-	Phone        string `gorm:"not null"`
-	Surname      string
-	Name         string `gorm:"not null"`
-	Patronymic   string
-	DateOfBirth  string
-	Address      string
-	PasswordHash string `gorm:"not null"`
-	CreatedAt    string
-	UpdatedAt    string
+	ID             uint   `gorm:"primary_key"`
+	Email          string `gorm:"unique;not null"`
+	Phone          string `gorm:"not null"`
+	Surname        string
+	Name           string `gorm:"not null"`
+	Patronymic     string
+	DateOfBirthday time.Time `gorm:"type:date"`
+	Address        string
+	PasswordHash   string `gorm:"not null"`
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
 }
 
 type UserRegistration struct {
 	Email               string `json:"email"`
 	Phone               string `json:"phone"`
-	FullName            string `json:"full_name"`
-	BirthDate           string `json:"birth_date"`
+	Name                string `json:"name"`
+	Surname             string `json:"surname"`
+	Patronymic          string `json:"patronymic"`
+	DateOfBirthday      string `json:"date_of_birthday"`
 	RegistrationAddress string `json:"registration_address"`
 	Password            string `json:"password"`
 }
@@ -36,22 +42,43 @@ func RegisterUser(c *gin.Context) {
 		return
 	}
 
+	dateOfBirthday, err := time.Parse(time.DateOnly, volunteer.DateOfBirthday)
+	if err != nil {
+		c.JSON(400, gin.H{"message": "Invalid date format"})
+		return
+	}
+
 	hashedPassword, err := utils.HashPassword(volunteer.Password)
 	if err != nil {
+		log.Println("Error hashing password:", err)
 		c.JSON(500, gin.H{"message": "Failed to hash password"})
 		return
 	}
 
 	user := User{
-		Email:        volunteer.Email,
-		Phone:        volunteer.Phone,
-		Name:         volunteer.FullName,
-		DateOfBirth:  volunteer.BirthDate,
-		Address:      volunteer.RegistrationAddress,
-		PasswordHash: hashedPassword,
+		Email:          volunteer.Email,
+		Phone:          volunteer.Phone,
+		Name:           volunteer.Name,
+		Surname:        volunteer.Surname,
+		Patronymic:     volunteer.Patronymic,
+		DateOfBirthday: dateOfBirthday,
+		Address:        volunteer.RegistrationAddress,
+		PasswordHash:   hashedPassword,
+		CreatedAt:      time.Now(),
+		UpdatedAt:      time.Now(),
 	}
 
 	if err := db.DB.Create(&user).Error; err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) {
+			log.Println("PostgreSQL error:", pgErr.Message, "Code:", pgErr.Code)
+			if pgErr.Code == "23505" {
+				c.JSON(400, gin.H{"message": "Email is already taken"})
+				return
+			}
+		}
+
+		log.Println("Unexpected error:", err)
 		c.JSON(500, gin.H{"message": "Failed to register user"})
 		return
 	}
