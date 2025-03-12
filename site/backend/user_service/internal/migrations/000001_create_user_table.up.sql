3b1332d9ba8cbbe8077c1398983ee4a98fca41dd
den990 <koldyrev98@icloud.com>
2025-03-12 11:42:14 +0300

    backend: Доделал все миграции

--- site/backend/user_service/internal/migrations/000001_create_user_table.up.sql
+++ site/backend/user_service/internal/migrations/000001_create_user_table.up.sql
@@ -12,3 +12,6 @@ CREATE TABLE "user" (
     "created_at" TIMESTAMP DEFAULT (CURRENT_TIMESTAMP),
     "updated_at" TIMESTAMP DEFAULT (CURRENT_TIMESTAMP)
 );
+
+INSERT INTO "user" ("email", "phone", "surname", "name", "patronymic", "date_of_birthday", "address", "password_hash", "is_admin", "created_at", "updated_at")
+VALUES ('admin@example.com', '+1234567890', 'Admin', 'Admin', 'Admin', NULL, NULL, '$2b$12$KDptV22sIJx4TFP3/wmj3OR6tEP0VE3jYKhQW2FvUuyS99Mo4U/fm', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
