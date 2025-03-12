2dd4963dc691f31741ebd66d5b3b03c8a781fa87
den990 <koldyrev98@icloud.com>
2025-03-12 12:20:31 +0300

    backend: Обновил хеш

--- site/backend/user_service/internal/migrations/000001_create_user_table.up.sql
+++ site/backend/user_service/internal/migrations/000001_create_user_table.up.sql
@@ -14,4 +14,4 @@ CREATE TABLE "user" (
 );
 
 INSERT INTO "user" ("email", "phone", "surname", "name", "patronymic", "date_of_birthday", "address", "password_hash", "is_admin", "created_at", "updated_at")
-VALUES ('admin@example.com', '+1234567890', 'Admin', 'Admin', 'Admin', NULL, NULL, '$2b$12$KDptV22sIJx4TFP3/wmj3OR6tEP0VE3jYKhQW2FvUuyS99Mo4U/fm', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES ('admin@example.com', '+1234567890', 'Admin', 'Admin', 'Admin', NULL, NULL, '$2a$10$G525BjR1Af0GV0yve/1jOu8LTZvCEQS/5p1K48Bv7MpUdBPdqLXl2', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
