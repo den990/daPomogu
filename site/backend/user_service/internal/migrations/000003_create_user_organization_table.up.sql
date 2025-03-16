5cb3f60f69418b6525528182ed83c1933b54dfb1
den990 <koldyrev98@icloud.com>
2025-03-16 15:57:11 +0300

    backend: Поставил task_service, но пока не связал его с другим сервисом

--- site/backend/user_service/internal/migrations/000003_create_user_organization_table.up.sql
+++ site/backend/user_service/internal/migrations/000003_create_user_organization_table.up.sql
@@ -3,5 +3,6 @@ CREATE TABLE "user_organization" (
     "user_id" INTEGER NOT NULL,
     "organization_id" INTEGER NOT NULL,
     "is_owner" BOOLEAN DEFAULT false,
+    "is_coordinator" BOOLEAN DEFAULT false,
     "is_accepted" BOOLEAN DEFAULT false
 );
