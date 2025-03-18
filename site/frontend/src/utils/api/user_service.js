4891599a98d7b2a065ac7a3a5e03e39727aba737
Aleksandr Mikhaylov <funnybulletgames@yandex.ru>
2025-03-18 14:50:43 +0300

    Добавил получение пользователей 1 страницы пагинации

--- site/frontend/src/utils/api/user_service.js
+++ site/frontend/src/utils/api/user_service.js
@@ -104,9 +104,9 @@ class UserServiceApi {
     });
   }
 
-  getAllUsersAndOrganizations(token) {
+  getUsersAndOrganizationsWithPagination(token, id) {
     this._updateToken(token);
-    return this._request(`${this._baseUrl}/organizations-users-list`, {
+    return this._request(`${this._baseUrl}/organizations-users-list/${id}`, {
         method: 'GET',
         headers: this._headers,
     });
