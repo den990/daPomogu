aae8fee18745b7802ee9fd8de56a873e4bc261f1
ZennoTime <99843517+ZennoTime@users.noreply.github.com>
2025-03-21 16:50:28 +0300

    немного подправил, но требует доработки

--- site/frontend/src/screens/AdminDashboard.js
+++ site/frontend/src/screens/AdminDashboard.js
@@ -1,20 +1,24 @@
 import { useState } from 'react';
 import Sidebar from "../components/adminDashboard/sidebar";
 import MainContent from "../components/adminDashboard/mainContent";
+import RoleHeader from "../components/RoleHeader/RoleHeader";
 
 function AdminDashboard() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   return (
-    <div className="h-full text-base-content">
-      <div id="admin-dashboard" className="min-h-screen bg-gray-50">
+    <div className="bg-gray-50 min-h-screen flex flex-col">
+      <RoleHeader />
+      <div className="flex flex-1 relative">
         <Sidebar 
           isOpen={isMobileMenuOpen} 
-          onClose={() => setIsMobileMenuOpen(false)}
+          setIsOpen={setIsMobileMenuOpen}
         />
-        <MainContent 
-          onMenuToggle={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-        />
+        <div className="flex-1 md:ml-64 overflow-hidden">
+          <MainContent 
+            onMenuToggle={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+          />
+        </div>
       </div>
     </div>
   );
