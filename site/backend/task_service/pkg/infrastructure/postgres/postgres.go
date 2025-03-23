d605a58749a09bce111f34a67acad323a5ab022b
den990 <koldyrev98@icloud.com>
2025-03-23 13:13:14 +0300

    backend: правки

--- site/backend/task_service/pkg/infrastructure/postgres/postgres.go
+++ site/backend/task_service/pkg/infrastructure/postgres/postgres.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/lib/pq"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 type Config struct {
@@ -17,7 +18,12 @@ type Config struct {
 }
 
 func NewPostgresGormDB(cfg Config) (*gorm.DB, error) {
-	dsn := fmt.Sprintf("user=%s password=%s dbname=%s host=%s sslmode=disable port=%s", cfg.Username, cfg.Password, cfg.DBName, cfg.Host, cfg.Port)
+	postgresUser := os.Getenv("POSTGRES_USER")
+	postgresPassword := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	postgresHost := "postgres-service"
+
+	dsn := fmt.Sprintf("user=%s password=%s dbname=%s host=%s sslmode=disable port=5432", postgresUser, postgresPassword, dbName, postgresHost)
 
 	var err error
 	dbg, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
