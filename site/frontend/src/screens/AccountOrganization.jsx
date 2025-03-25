4aa6a5c6971d0d9517d6efffdd620729ae9005aa
Aleksandr Mikhaylov <funnybulletgames@yandex.ru>
2025-03-25 11:31:34 +0300

    Удалил лишнее

--- site/frontend/src/screens/AccountOrganization.jsx
+++ site/frontend/src/screens/AccountOrganization.jsx
@@ -1,6 +1,5 @@
 import Profile from "../components/accountOrganization/profile.jsx";
 import ActiveTasks from "../components/accountOrganization/activeTasks.jsx";
-import Volunteers from "../components/accountOrganization/volunteers.jsx";
 import Stats from "../components/accountOrganization/stats.jsx";
 import RoleHeader from "../components/RoleHeader/RoleHeader.js";
 
@@ -11,7 +10,6 @@ function AccountOrganization() {
             <main className="max-w-7x1 mx-auto px-4 sm:px-6 lg:px-8 py-8">
                 <Profile />
                 <ActiveTasks />
-                <Volunteers />
                 <Stats />
             </main>
         </div>
